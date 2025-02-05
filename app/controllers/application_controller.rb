972505cbdf7aa03779aba58e77a593c422d28689
Brent Odell <brentdodell@gmail.com>
2025-02-05 08:59:38 -0600

    Update `allow_browser` to be more lax
    - My current version of Safari (16.6) wasn't allowed. I can update my browser,
      but if I'm running into this, less technical folks certainly will.
    - Allow browsers that can support Tailwind CSS 4.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   include Authentication
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+
+  # Only allow modern browsers supporting features used by TailwindCSS 4.
+  #
+  # Safari, Chrome, and Firefox values came from https://tailwindcss.com/docs/compatibility
+  #
+  # I left Opera with the current "modern" default defined by Rails. Opera 104 seems to be
+  # based on Chromium 118, and was released October 2023, so I think we're safe.
+  # https://en.wikipedia.org/wiki/History_of_the_Opera_web_browser
+  allow_browser versions: { safari: 16.4, chrome: 111, firefox: 128, opera: 104, ie: false }
 end
